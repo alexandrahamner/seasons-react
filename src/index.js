f69293bfb112cb7bed1f23bb2c2f59082181e09a
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-12-15 21:49:07 -0600

    conditionally rendering the state of my app comp.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    // to initialize our state object
     constructor (props) {
         //necessary
         super(props);
 
-        this.state = {};
+        this.state = {lat: null, errorMessage: ''};
+
+        window.navigator.geolocation.getCurrentPosition(
+            // Callback #1 = Success Callback; everything goes as planned
+            (position) => {
+                //to update our state, we call setSatelo
+                this.setState({ lat: position.coords.latitude });
+            },
+            // Failure callback; get current position is unable to determine position.
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
     }
     //Have to define render
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            // Callback #1 = Success Callback; everything goes as planned
-            (position) => console.log(position), 
-            // Failure callback; get current position is unable to determine position.
-            (err) => console.log(err)
-        );
-        return <div>Latitude: </div>
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        } 
+
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        
+        return <div>Loading!</div>
+
     }
 }
 
