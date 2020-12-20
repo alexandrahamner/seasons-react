a99e2f00066905a9b59166d5fe1a462a569d387a
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-12-19 23:04:39 -0600

    feat: added icons to the page and cleaned up the code, including adding a seasonConfig function.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach!',
+        icon: 'sun'
+    },
+    winter: {
+        text: 'Burr, it\'s cold outside!',
+        icon: 'snowflake'
+    }
+};
+
 const getSeason = (lat, month) => {
     if(month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
     } else {
         return lat > 0 ? 'winter' : 'summer';
     }
-}
+};
 
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    console.log(season);
-    return <div>Season Display</div>;
+    const {text, iconName} = seasonConfig[season]
+    return (
+        <div>
+            <i className = {`huge ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className = {`huge ${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
